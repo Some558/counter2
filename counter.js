9f396045f2996372862d1c15edee1bcfe0f12d82
Some558 <167545573+Some558@users.noreply.github.com>
2024-06-12 14:23:11 +0900

    カウントが3の倍数のとき!マークを表示させる

--- counter.js
+++ counter.js
@@ -9,9 +9,16 @@
         }else{
             $counter.textContent = currentCount -1;
         }
-    }
+        const $message = document.getElementById("message");
+        if($counter.textContent % 3 === 0 ) {
+            $message.textContent = "!";
+        } else {
+            $message.textContent = "";
+        }
+    };
+
     for(let index = 0; index < document.getElementsByClassName("js-button").length; index++){
         document.getElementsByClassName("js-button")[index].
     addEventListener("click",(e) => clickHandler(e))
-    }
+    }    
 })();
